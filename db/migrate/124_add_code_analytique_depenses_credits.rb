b7b02ab554f19f0854a54ec584fd941b24c7ab69
Benjamin Ninassi <benjamin.ninassi@gmail.com>
2016-04-26 14:51:21 +0200

    code_analytique and compte_budgetaire migrations
    Adding code_analytique and compte_budgetaire to models

---
+++ db/migrate/124_add_code_analytique_depenses_credits.rb
@@ -0,0 +1,21 @@
+class AddCodeAnalytiqueDepensesCredits < ActiveRecord::Migration
+  def self.up
+    add_column :depense_non_ventilees, :code_analytique, :string
+    add_column :depense_fonctionnements, :code_analytique, :string
+    add_column :depense_equipements, :code_analytique, :string
+    add_column :depense_missions, :code_analytique, :string
+    add_column :depense_salaires, :code_analytique, :string
+    add_column :depense_commun, :code_analytique, :string
+    add_column :versement, :code_analytique, :string
+  end
+
+  def self.down
+    remove_column :depense_non_ventilees, :code_analytique
+    remove_column :depense_fonctionnements, :code_analytique
+    remove_column :depense_equipements, :code_analytique
+    remove_column :depense_missions, :code_analytique
+    remove_column :depense_salaires, :code_analytique
+    remove_column :depense_commun, :code_analytique
+    remove_column :versement, :code_analytique
+  end
+end
---
+++ db/migrate/125_add_compte_budgetaire_depenses_credits.rb
@@ -0,0 +1,21 @@
+class AddCompteBudgetaireDepensesCredits < ActiveRecord::Migration
+  def self.up
+    add_column :depense_non_ventilees, :compte_budgetaire, :string
+    add_column :depense_fonctionnements, :compte_budgetaire, :string
+    add_column :depense_equipements, :compte_budgetaire, :string
+    add_column :depense_missions, :compte_budgetaire, :string
+    add_column :depense_salaires, :compte_budgetaire, :string
+    add_column :depense_commun, :compte_budgetaire, :string
+    add_column :versement, :compte_budgetaire, :string
+  end
+
+  def self.down
+    remove_column :depense_non_ventilees, :compte_budgetaire
+    remove_column :depense_fonctionnements, :compte_budgetaire
+    remove_column :depense_equipements, :compte_budgetaire
+    remove_column :depense_missions, :compte_budgetaire
+    remove_column :depense_salaires, :compte_budgetaire
+    remove_column :depense_commun, :compte_budgetaire
+    remove_column :versement, :compte_budgetaire
+  end
+end
