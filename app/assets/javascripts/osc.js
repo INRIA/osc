a1fb4f58de32090a636142bf032d09cd18e13af0
Benjamin Ninassi <benjamin.ninassi@gmail.com>
2018-01-31 11:54:46 +0100

    adding HttpOnly to cookies for security

--- app/assets/javascripts/osc.js
+++ app/assets/javascripts/osc.js
@@ -25,7 +25,7 @@ function setCookie(c_name,value,exdays)
 {
   var exdate=new Date();
   exdate.setDate(exdate.getDate() + exdays);
-  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString())+"; path=/";
+  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString())+"; path=/; HttpOnly"; 
   document.cookie=c_name + "=" + c_value;
 }
 
